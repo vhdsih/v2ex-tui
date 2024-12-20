f942a6082dfa89d190b3e655b0a4d5bbb5e37c68
kaolengmian7 <kaolengmian7@qq.com>
2024-12-20 17:24:11 +0800

    feature:go build

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Windows
-GOOS=windows GOARCH=amd64 go build -o bin/windows_v2ex_tui.exe ./cmd/v2ex/main.go
+#GOOS=windows GOARCH=amd64 go build -o bin/windows_v2ex_tui.exe ./cmd/v2ex/main.go
 
 # Mac Intel
 GOOS=darwin GOARCH=amd64 go build -o bin/mac_v2ex_tui ./cmd/v2ex/main.go
 
 # Linux
-GOOS=linux GOARCH=amd64 go build -o bin/linux_v2ex_tui ./cmd/v2ex/main.go
+#GOOS=linux GOARCH=amd64 go build -o bin/linux_v2ex_tui ./cmd/v2ex/main.go
